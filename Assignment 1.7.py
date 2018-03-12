9c4f7a6c5d6902fdc11797a2629539f4d0e49ca3
zabiullakhangithub <37284769+zabiullakhangithub@users.noreply.github.com>
2018-03-12 12:50:21 +0530

    Add files via upload

---
+++ Assignment 1.7.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 22 12:46:45 2018
+
+@author: zabiulla.khan
+
+Write a Python program to reverse a word after accepting the input from the user.
+Sample Output:
+Input word: AcadGild
+Output: dilGdacA
+
+"""
+# Accept a word from user input
+word = str(input("Enter any word to reverse:"))
+
+#print the word in reverse order
+print(word[::-1])
